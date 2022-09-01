03501976c1073bc085b76eede74fa91bb9ff5cb5
lisong <song.li@gaea.com>
2022-09-01 13:03:53 +0800

    update

---
+++ ios 2017.4.40c1.sh
@@ -0,0 +1,12 @@
+
+unity_path='/Applications/Unity/Hub/Editor/2017.4.40c1/Unity.app'
+
+project_path='/Users/lisong/Desktop/Project_test/UnityProjects/Unity-UIAnimeTool'
+
+build_target='ios'
+
+
+
+open -n $unity_path --args  -projectPath $project_path -buildTarget $build_target 
+
+exit 0
